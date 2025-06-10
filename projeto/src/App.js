6f34767f0bfb5297266d9656d74f5d64b0a6c05e
Zhou <141890618+RobertoZhou@users.noreply.github.com>
2025-06-09 21:50:01 -0300

    Curso React: Navbar e footer

--- projeto/src/App.js
+++ projeto/src/App.js
@@ -1,27 +1,26 @@
-// filepath: c:\Users\Zhou\Documents\GitHub\React-Gerenciador_de_Projetos\projeto\src\App.js
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './components/pages/Home.js';
 import Contact from './components/pages/Contact.js';
 import Company from './components/pages/Company.js';
 import NewProject from './components/pages/NewProject.js';
+import Projects from './components/pages/Projects.js';
+
 import Container from './components/layout/Container.js';
+import Navbar from './components/layout/Navbar.js';
+import Footer from './components/layout/Footer.js';
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to='/'>Home</Link>
-        <Link to='/contact'>Contact</Link>
-        <Link to='/company'>Company</Link>
-        <Link to='/newproject'>NewProject</Link>
-      </div>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Container customClass='min-height'><Home /></Container>} />
+        <Route path='/projects' element={<Container customClass='min-height'><Projects /></Container>} />
         <Route path='/company' element={<Container customClass='min-height'><Company /></Container>} />
         <Route path='/contact' element={<Container customClass='min-height'><Contact /></Container>} />
         <Route path='/newproject' element={<Container customClass='min-height'><NewProject /></Container>} />
       </Routes>
-      <p>Footer</p>
+      <Footer />
     </Router>
   );
 }
