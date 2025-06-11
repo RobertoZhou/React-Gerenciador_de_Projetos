d90d603fc3e67e9282405cb94b9691515b609b87
Zhou <141890618+RobertoZhou@users.noreply.github.com>
2025-06-11 18:50:10 -0300

    Página para o formulário de projeto

---
+++ projeto/src/components/projects/ProjectForm.js
@@ -0,0 +1,20 @@
+const ProjectForm = () => {
+    return(
+        <form>
+            <div>
+                <input type="text" placeholder="Insira o nome do projeto" />
+            </div>
+            <div>
+                <input type="number" placeholder="Insira o orçamento total" />
+            </div>
+            <div>
+                <select name="category_id" >
+                    <option>Selecione a categoria</option>
+                </select>
+            </div>
+        </form>
+    )
+        
+}
+
+export default ProjectForm;
